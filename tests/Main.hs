2a7d59e68ad7ba3dd5d5d804ae0cf63a1c48ef4a
Sergey Petrov <git@pr0ger.org>
2020-08-02 22:21:22 +0300

    add tests target

---
+++ tests/Main.hs
@@ -0,0 +1,17 @@
+module Main where
+
+import Test.Tasty
+import Test.Tasty.Hspec
+
+main :: IO()
+main = do
+    hspecTree <- spec
+    defaultMain $ testGroup "Tests" 
+        [
+            hspecTree
+        ]
+
+spec :: IO TestTree
+spec = testSpec "hspec" $ describe "test" $
+    it "5 == 5" $
+        5 `shouldBe` (5 :: Int)
