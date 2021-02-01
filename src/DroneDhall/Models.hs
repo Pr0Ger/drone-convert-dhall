d00ae1e4d9b79664bbddc395cfd563308f5ae229
Sergey Petrov <git@pr0ger.org>
2021-02-01 03:30:29 +0300

    Simplify DroneDhall.Models

--- src/DroneDhall/Models.hs
+++ src/DroneDhall/Models.hs
@@ -1,39 +1,26 @@
 {-# LANGUAGE DeriveGeneric         #-}
 {-# LANGUAGE DerivingStrategies    #-}
-{-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE OverloadedStrings     #-}
 
-module DroneDhall.Models where
+module DroneDhall.Models
+  ( Config(..)
+  , Request(..)
+  ) where
 
 import Data.Aeson
 import GHC.Generics (Generic)
 
-data Config = Config
-  { data' :: String
-  , kind :: String
-  }
+newtype Config = Config { data' :: String }
   deriving stock (Eq, Show)
 
-data Repository = Repository
-  { id :: Integer
-  , trusted :: Bool
-  , protected :: Bool
-  }
-  deriving stock (Eq, Show, Generic)
-
-data Request = Request
-  { config :: Config
-  , repo :: Repository
-  }
-  deriving (Show)
-
+newtype Request = Request { config :: Config }
+  deriving stock (Generic, Show)
 
 instance FromJSON Config where
   parseJSON (Object v) = Config
     <$> v .: "data"
-    <*> v .:? "kind" .!= ""
-
-instance FromJSON Repository
 
 instance ToJSON Config where
-  toJSON (Config data' _) = object ["data" .= data']
+  toJSON (Config data') = object ["data" .= data']
+
+instance FromJSON Request
--- tests/Models.hs
+++ tests/Models.hs
@@ -1,33 +1,33 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Models
-    ( spec
-    ) where
+  ( spec
+  ) where
 
 import Data.Aeson        (decode, encode)
-import DroneDhall.Models (Config (..), Repository (..))
+import DroneDhall.Models (Config (..), Request (..))
 import Test.Tasty        (TestTree)
 import Test.Tasty.Hspec  (describe, it, shouldBe, testSpec)
 
 spec :: IO TestTree
 spec = testSpec "json models" $ do
-    describe "config" $ do
-        it "should be parsable from json" $
-            let
-                jsonText = "{\"data\": \"some data\"}"
-                json     = decode jsonText :: Maybe Config
-            in data' <$> json `shouldBe` Just "some data"
+  describe "config" $ do
+    it "should be parsable from json" $
+      let
+        jsonText = "{\"data\": \"some data\"}"
+        json     = decode jsonText :: Maybe Config
+      in data' <$> json `shouldBe` Just "some data"
 
-        it "should be encodable to json" $
-            let
-                obj          = Config "some data" ""
-                jsonText     = encode obj
-                expectedText = "{\"data\":\"some data\"}"
-            in jsonText `shouldBe` expectedText
+    it "should be encodable to json" $
+      let
+        obj          = Config "some data"
+        jsonText     = encode obj
+        expectedText = "{\"data\":\"some data\"}"
+      in jsonText `shouldBe` expectedText
 
-    describe "repository" $
-        it "should be parsable from json" $ do
-            let jsonText = "{\"id\": 0, \"trusted\": false, \"protected\": false}"
-                json     = decode jsonText :: Maybe Repository
-
-            trusted <$> json `shouldBe` Just False
+  describe "request" $ do
+    it "should be parsable from json" $
+      let
+        jsonText = "{\"config\": {\"data\": \"test config file\"}}"
+        json     = decode jsonText :: Maybe Request
+      in data' . config <$> json `shouldBe` Just "test config file"
